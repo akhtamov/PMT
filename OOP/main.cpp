7afbb8ac7c28896bbc6985d983f44128c9456f58
Vlad <avol33t>
2020-04-30 19:52:36 +0700

    /

--- OOP/main.cpp
+++ OOP/main.cpp
@@ -3,16 +3,16 @@
 
 //
 int main(int argc, char* argv[]) {
-    string inputPath = "input.txt";
-    string outputPath = "output.txt";
+    string inputPath;
+    string outputPath;
 
- //   if (argc != 3)
-	//{
-	//	cout << "Type input and output files\n";
-	//	return 1;
-	//}
-	//inputPath = argv[1];
-	//outputPath = argv[2];
+    if (argc != 3)
+	{
+		cout << "Type input and output files\n";
+		return 1;
+	}
+	inputPath = argv[1];
+	outputPath = argv[2];
 
     ifstream infile(inputPath);
     if (!infile.is_open())
--- Procedure/main.cpp
+++ Procedure/main.cpp
@@ -2,7 +2,6 @@
 #include <string>
 #include "container.h"
 using namespace std;
-
 int main(int argc, char* argv[])
 {
     string outputPath;
